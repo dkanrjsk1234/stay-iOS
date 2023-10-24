9a49b1625224b8dc1ec5b6cb34acd1e6a4afb72d
Kim ju hwan <minecraft_bos@naver.com>
2023-10-24 17:27:21 +0900

    📄:: add file
    파일 추가

---
+++ stay/My/MypageView.swift
@@ -0,0 +1,18 @@
+//
+//  MypageView.swift
+//  stay
+//
+//  Created by dgsw8th26 on 10/24/23.
+//
+
+import SwiftUI
+
+struct MypageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MypageView()
+}
---
+++ stay/Travelcourse/TravelcourseView.swift
@@ -0,0 +1,18 @@
+//
+//  TravelcourseView.swift
+//  stay
+//
+//  Created by dgsw8th26 on 10/24/23.
+//
+
+import SwiftUI
+
+struct TravelcourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TravelcourseView()
+}
